c1e589d3023b1331a5df31caf970afbc2633723b
Nate Leavitt <toomanynates@yahoo.com>
2026-09-10 17:51:34 -0700

    finishing project

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
-APIs:
+// APIs:
 
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+function renderPosts(id) {
+    // store id to local storage
+    sessionStorage.setItem("userId", id);
+
+    window.location.href = `${window.location.origin}/user.html`;
+}
+
+function renderCards(users) {
+    /* make a loop that renders each user card like this:
+        <div class="user-card">
+        <div class="user-card__container">
+            <h3>User's Name</h3>
+            <p><b>Email:</b> email@email.com</p>
+            <p><b>Phone:</b> 0000000000</p>
+            <p><b>Website: </b> <a href="https://website.website" target="_blank">website.website</a></p>
+        </div>
+        </div>
+    */
+    let usersHtml = "";
+    for (const user of users) {
+        usersHtml += `
+        <div class="user-card" onclick="renderPosts(${user.id})">
+            <div class="user-card__container">
+                <h3>${user.name}</h3>
+                <p><b>Email:</b> ${user.email}</p>
+                <p><b>Phone:</b> ${user.phone}</p>
+                <p><b>Website: </b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+            </div>
+        </div>
+        `;
+    }
+    document.querySelector(".user-cards").innerHTML = usersHtml;
+}
+
+async function main() {   
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+
+    console.log(usersData[0]);
+
+    renderCards(usersData);
+}
+
+main();
--- user.js
+++ user.js
@@ -1,1 +1,42 @@
-// user.js code goes in here
+
+let userId = sessionStorage.getItem("userId");
+const postList = document.querySelector(".post-list");
+const searchInput = document.querySelector(".post__search--input");
+
+searchInput.value = userId ?? ""; // Use fallback only when value is null or undefined.
+
+function onSearchChange(event)
+{
+    const searchValue = event.target.value;
+    console.log("onSearchChange() user.js - searchValue:", searchValue);
+    userId = searchValue;
+    getPosts(userId);
+}
+
+// render the posts using the array map function. getItemById post-list and then render this:
+function renderPosts(posts) {
+    const postsHtml = posts.map(post => `
+        <div class="post">
+          <div class="post__title">
+            ${post.title}
+          </div>
+          <p class="post__body">
+            ${post.body}
+          </p>
+        </div>
+      `).join('');
+    postList.innerHTML = postsHtml;
+}
+
+// inspect session storage for id and console log it to make sure it is being stored correctly
+async function getPosts(userId) {
+    console.log("main() user.js - userId:", userId);
+
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    const postsData = await posts.json();   
+
+    console.log(postsData);
+    renderPosts(postsData);
+}
+
+getPosts(userId);
